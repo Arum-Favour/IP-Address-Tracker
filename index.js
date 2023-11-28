37e61a2827f4e45a43dd54922ed1bcaafbe738cc
Kingson.Arum <nwajiudo@gmail.com>
2023-11-28 12:54:31 +0100

    commit

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-async function getLocation(){
-    const getLocation = await fetch("https://geo.ipify.org/api/v2/country,city?apiKey=at_3fejZuvyag7r3eetqnNtHM3ApC2Xi&domain=www.google.com");
+alert("The App Just Began")
+async function getLocation() {
+  let Input_text = document.querySelector("input");
+  let Ip_address;
+  let domain_name;
+  let regex = "www..*.com";
+
+  if (Input_text.value.match(regex)) {
+    domain_name = Input_text;
+    const getLocation = await fetch(
+      `https://geo.ipify.org/api/v2/country,city?apiKey=at_3fejZuvyag7r3eetqnNtHM3ApC2Xi&domain= ${domain_name}`
+    );
     const result = await getLocation.json();
-    console.log(result);
+  }else{
+    Ip_address=Input_text;
+    const getLocation = await fetch(
+        `https://geo.ipify.org/api/v2/country,city?apiKey=at_3fejZuvyag7r3eetqnNtHM3ApC2Xi&ipAddress= ${Ip_address}`
+      );
+      const result = await getLocation.json();
+  }
+
+  console.log(result);
 }
 getLocation();
