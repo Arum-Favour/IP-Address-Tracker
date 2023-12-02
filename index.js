1b0c0c04fbbbbe0d7f31d3ebb407cb61f122844e
Kingson.Arum <nwajiudo@gmail.com>
2023-12-02 21:23:27 +0100

    commit

--- index.js
+++ index.js
@@ -1,55 +1,40 @@
-let map;
 let Ip_address;
 let domain_name;
 let latitude;
 let longitude;
-let marker;
-let Input_text = document.querySelector("input");
 let Ipresult = document.querySelector(".Ipresult");
 let ipLocation = document.querySelector(".Loc_result");
 let timeResult = document.querySelector(".time_result");
 let ispResult = document.querySelector(".ispresult");
 
-function initMap() {
-  map = L.map("map").setView([latitude, longitude], 13);
-  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 19,
-    attribution:
-      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-  }).addTo(map);
-}
+displayIp();
+let map = L.map("map").setView([latitude, longitude], 13);
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
 
-function addMarker(latitude, longitude) {
-  marker = L.marker([latitude, longitude]).addTo(map);
-  return marker;
-}
-
-function updateMarker(marker, latitude, longitude) {
-  marker.setLatLng([latitude, longitude]);
-  // return marker
-}
+let marker = L.marker([latitude, longitude]).addTo(map);
 
 //CODE THAT DISPLAYS THE IP ADDRESS DETAILS AND MAP AT LOAD
-async function displayIpMap() {
+async function displayIp() {
   const getLocation = await fetch(
     `https://geo.ipify.org/api/v2/country,city?apiKey=at_3fejZuvyag7r3eetqnNtHM3ApC2Xi`
   );
   const result = await getLocation.json();
   Ipresult.innerHTML = result.ip;
-  ipLocation.innerHTML = result.location.region + "," + result.location.city;
+  ipLocation.innerHTML = result.location.region + "," +result.location.city;
   timeResult.innerHTML = "UTC " + result.location.timezone;
   ispResult.innerHTML = result.isp;
   longitude = result.location.lng;
   latitude = result.location.lat;
   console.log(longitude, latitude);
-
-  if (!map) {
-    initMap();
-  }
 }
 
 //CODE TO GET LOCATION AND IP ADRESS DETAILS OF SEARCH
-async function getLocation() {
+async function updateMapLocation() {
+  let Input_text = document.querySelector("input");
   let regex = /www\..*\.com/i;
   let anotherRegex = /\.com/i;
 
@@ -60,6 +45,7 @@ async function getLocation() {
       `https://geo.ipify.org/api/v2/country,city?apiKey=at_3fejZuvyag7r3eetqnNtHM3ApC2Xi&domain=${domain_name}`
     );
     const result = await getLocation.json();
+
     Ipresult.innerHTML = result.ip;
     ipLocation.innerHTML = result.location.region + "," + result.location.city;
     timeResult.innerHTML = "UTC " + result.location.timezone;
@@ -67,11 +53,14 @@ async function getLocation() {
     longitude = result.location.lng;
     latitude = result.location.lat;
 
-    if (!map) {
-      initMap();
-    }
+    map.whenReady(function () {
+      if (latitude && longitude) {
+        map.setView([latitude, longitude], 13);
+        marker.setLatLng([latitude, longitude]);
+      }
+    });
 
-        
+    // map.setView([latitude, longitude], 13);
   } else {
     Ip_address = Input_text.value;
     const getLocation = await fetch(
@@ -88,13 +77,13 @@ async function getLocation() {
     let latitude = result.location.lat;
     console.log(longitude, latitude);
 
-    var map = L.map("map").setView([latitude, longitude], 13);
-    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      maxZoom: 19,
-      attribution:
-        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    }).addTo(map);
+    // var map = L.map("map").setView([latitude, longitude], 13);
+    // L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    //   maxZoom: 19,
+    //   attribution:
+    //     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    // }).addTo(map);
 
-    var marker = L.marker([latitude, longitude]).addTo(map);
+    // var marker = L.marker([latitude, longitude]).addTo(map);
   }
 }
