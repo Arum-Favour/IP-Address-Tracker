36d8b6665e22b14c5297d323e822d029a84acde9
Kingson.Arum <nwajiudo@gmail.com>
2023-12-04 23:05:46 +0100

    made some more commits

--- index.js
+++ index.js
@@ -77,19 +77,20 @@ async function updateMapLocation() {
     ipLocation.innerHTML = result.location.region;
     timeResult.innerHTML = "UTC " + result.location.timezone;
     ispResult.innerHTML = result.isp;
-    console.log(Ip_address);
-
     let longitude = result.location.lng;
     let latitude = result.location.lat;
-    console.log(longitude, latitude);
 
-    // var map = L.map("map").setView([latitude, longitude], 13);
-    // L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    //   maxZoom: 19,
-    //   attribution:
-    //     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    // }).addTo(map);
+    if (map) {
+      map.off();
+      map.remove();
+    }
+    map = L.map("map").setView([latitude, longitude], 13);
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      maxZoom: 19,
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(map);
 
-    // var marker = L.marker([latitude, longitude]).addTo(map);
+    marker = L.marker([latitude, longitude]).addTo(map);
   }
 }
